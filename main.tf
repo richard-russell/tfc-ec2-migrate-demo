dbc01882787d180f273b094c1d373063617efeda
Richard Russell <richard.russell@hashicorp.com>
2023-11-06 10:13:12 +0000

    Add outputs

--- main.tf
+++ main.tf
@@ -45,3 +45,15 @@ resource "aws_instance" "web" {
     Name = "migrate-me"
   }
 }
+
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.my_subnet.id
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
